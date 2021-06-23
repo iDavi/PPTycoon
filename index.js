fdce0bebe06bc0532cdd805df79216474e675f78
iDavi <69827032+iDavi@users.noreply.github.com>
2021-06-23 14:59:16 -0300

    Versão 0.0.0.1 - Absolutamente nada Nada funciona ainda

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const api = require("./api/api.js");
+const registro = require("./sistema/registro.js");
+const Discord = require("discord.js");
+const client = new Discord.client();
+
+client.login(process.env.TOKEN);
